c102731d11836ac763510f9a60ba34175b991da4
Ryle <rylemay198@gmail.com>
2025-09-16 20:52:37 +0200

    added network prac

---
+++ NetworkingPrac/src/main/java/za/ac/cput/networkingprac/ClientApp.java
@@ -0,0 +1,66 @@
+package za.ac.cput.networkingprac;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 230333907
+ */
+public class ClientApp {
+
+    Scanner sc = new Scanner(System.in);
+    private Socket server;
+
+    /**
+     * Creates a new instance of ClientApp
+     */
+    public ClientApp() {
+        // Attempt to establish connection to server
+        try {
+            // Create socket
+            server = new Socket("127.0.0.1", 6666);
+        } catch (IOException ioe) {
+            System.out.println("IOException: " + ioe.getMessage());
+        }
+    }
+
+    public void communicate() {
+        // The connection has been established - now send/receive.
+
+        try {
+            // Step 1: create channels
+            ObjectOutputStream out = new ObjectOutputStream(server.getOutputStream());
+            out.flush();
+            ObjectInputStream in = new ObjectInputStream(server.getInputStream());
+
+            // Step 2: communicate
+            
+            String msg="";
+            while (!msg.equals("exit")) {
+                msg = sc.nextLine();
+                out.writeObject(msg);
+                out.flush();
+                String response = (String) in.readObject();
+                System.out.println("From SERVER>> " + response);
+            }
+
+            // Step 3: close down
+            out.close();
+            in.close();
+            server.close();
+        } catch (IOException ioe) {
+            System.out.println("IO Exception: " + ioe.getMessage());
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Class not found: " + cnfe.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ClientApp client = new ClientApp();
+        client.communicate();
+    }
+}
---
+++ NetworkingPrac/src/main/java/za/ac/cput/networkingprac/ServerApp.java
@@ -0,0 +1,81 @@
+package za.ac.cput.networkingprac;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
+
+/**
+ *
+ * @author 230333907
+ */
+public class ServerApp {
+
+    // Server socket
+    private ServerSocket listener;
+
+    // Client connection
+    private Socket client;
+
+    /**
+     * Creates a new instance of ServerApp
+     */
+    public ServerApp() {
+        // Create server socket
+        try {
+            listener = new ServerSocket(6666, 1);
+        } catch (IOException ioe) {
+            System.out.println("IO Exception: " + ioe.getMessage());
+        }
+    }
+
+    public void listen() {
+        // Start listening for client connections
+        try {
+            System.out.println("Server is listening");
+            client = listener.accept();
+            System.out.println("Now moving onto processClient");
+
+            processClient();
+        } catch (IOException ioe) {
+            System.out.println("IO Exception: " + ioe.getMessage());
+        }
+    }
+
+    public void processClient() {
+        // Communicate with the client
+
+        // First step: initiate channels
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+            out.flush();
+            ObjectInputStream in = new ObjectInputStream(client.getInputStream());
+
+            // Step 2: communicate
+            String msg="";
+            while (!msg.equals("exit")) {
+                msg = (String) in.readObject();
+                System.out.println("From CLIENT>> " + msg);
+                out.writeObject("Hello " + msg);
+                out.flush();
+            }
+
+            // Step 3:close down
+            out.close();
+            in.close();
+            client.close();
+        } catch (IOException ioe) {
+            System.out.println("IO Exception: " + ioe.getMessage());
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Class not found: " + cnfe.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Create application
+        ServerApp server = new ServerApp();
+
+        // Start waiting for connections
+        server.listen();
+    }
+}
